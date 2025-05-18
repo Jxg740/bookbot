3dfb2d4368f2382fd10c399bff06e91d9e04f1f8
Joe <jxgierk740@gmail.com>
2025-05-18 15:35:30 -0600

    Add func get_num_char to count characters

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def main():    
     # Sets the path to the text
-    book_path = "books/frankenstein.txt"    
+    book_path = "books/frankenstein.txt" 
+
     # Calls get_book_text with book_path as parameter
     text = get_book_text(book_path)
+
     # Calls get_num_words from stats with text as parameter
     # Creates a variable to store it
     num_words = get_num_words(text)
+
+    # feeds text to get_num_char and assigns it to char_count
+    char_count = get_num_char(text)
+
     # Prints the number of words
     print(f"{num_words} words found in the document")
+    print(char_count)
+
+
+
 
 def get_book_text(path):
     # Takes path as parameter
--- stats.py
+++ stats.py
@@ -4,5 +4,15 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_num_char():
-    pass
+
+def get_num_char(text):
+    chars = list(text.lower())
+    chars_dict = {}
+    for char in chars:
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+    return chars_dict
+
+
