f1aa35e220bc217958ed96b60bb4fcdabcec6d20
Joe <jxgierk740@gmail.com>
2025-05-18 18:15:38 -0600

    Adding comments to stats.py

--- stats.py
+++ stats.py
@@ -15,13 +15,17 @@ def get_num_char(text):
             chars_dict[char] = 1
     return chars_dict
 
-
+# Helper function used for sorting; returns the 'num' value from a dictionary
 def sort_on(d):
     return d["num"]
 
+# Function to convert a dictionary of character counts into a sorted list of dictionaries
 def sorted_char_list(dict):
     dict_list = []
+    # Convert each character and its count into a dictionary and add it to a list
     for i in dict:
         dict_list.append({"char": i, "num": dict[i]})
+    # Sort the list of dictionaries in descending order based on the 'num' value
     dict_list.sort(reverse=True, key=sort_on)
+     # Return the sorted list
     return dict_list
